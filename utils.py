c4dbd2438c52915423803cc96da45b289066dbeb
Leo Cheng <leocheng333375749@gmail.com>
2024-10-19 15:18:12 -0400

    fix greyscale mask to boolean

--- utils.py
+++ utils.py
@@ -2,15 +2,16 @@ import numpy as np
 import cv2, zlib, base64, io
 from PIL import Image
 
-def base64_to_mask(s):
+def base64_2_mask(s):
     z = zlib.decompress(base64.b64decode(s))
-    n = np.frombuffer(z, np.uint8)
-    mask = cv2.imdecode(n, cv2.IMREAD_GRAYSCALE)
+    n = np.fromstring(z, np.uint8)
+    mask = cv2.imdecode(n, cv2.IMREAD_UNCHANGED)[:, :, 3].astype(bool)
     return mask
 
-def mask_to_base64(mask):
-    img_pil = Image.fromarray(np.array(mask, dtype=np.uint8), mode="L")
+def mask_2_base64(mask):
+    img_pil = Image.fromarray(np.array(mask, dtype=np.uint8))
+    img_pil.putpalette([0,0,0,255,255,255])
     bytes_io = io.BytesIO()
-    img_pil.save(bytes_io, format="PNG", transparency=0, optimize=0)
-    bytes_enc = bytes_io.getvalue()
-    return base64.b64encode(zlib.compress(bytes_enc)).decode("utf-8")
+    img_pil.save(bytes_io, format='PNG', transparency=0, optimize=0)
+    bytes = bytes_io.getvalue()
+    return base64.b64encode(zlib.compress(bytes)).decode('utf-8')
