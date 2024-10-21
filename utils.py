95b2033ede3cdeb320fc24ff43a175384007eb57
Leo Cheng <leocheng333375749@gmail.com>
2024-10-21 19:47:07 -0400

    setup venv + add get_images util

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2, zlib, base64, io
 from PIL import Image
-import glob
+import json
+import os
+
 
 def base64_2_mask(s):
     z = zlib.decompress(base64.b64decode(s))
@@ -9,16 +11,28 @@ def base64_2_mask(s):
     mask = cv2.imdecode(n, cv2.IMREAD_UNCHANGED)[:, :, 3].astype(bool)
     return mask
 
+
 def mask_2_base64(mask):
     img_pil = Image.fromarray(np.array(mask, dtype=np.uint8))
-    img_pil.putpalette([0,0,0,255,255,255])
+    img_pil.putpalette([0, 0, 0, 255, 255, 255])
     bytes_io = io.BytesIO()
     img_pil.save(bytes_io, format='PNG', transparency=0, optimize=0)
     bytes = bytes_io.getvalue()
     return base64.b64encode(zlib.compress(bytes)).decode('utf-8')
 
-def get_images(folder):
-    map(Image.open, glob(f'{folder}/img/*.png'))
-    # zip image (nparray) and output (0/1) later
+
+def get_images(folder, w, h):
+    images = []
+    json_data = []
+
+    try:
+        for f in os.listdir(f'{folder}/img'):
+            images.append(cv2.resize(cv2.imread(f'{folder}/img/{f}', cv2.IMREAD_GRAYSCALE), (w, h)))
+            with open(f'{folder}/ann/{f}.json', 'r') as file:
+                json_data.append(json.load(file))
+    except Exception as e:
+        print(e)
+
+    return images, json_data
 
 
