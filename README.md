346408f93f3454af9558019f374ebeba94a315be
reetkaurbhamra <reet.rkb@gmail.com>
2024-11-12 12:52:54 -0500

    attempt 2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Lungs have Tuberculosis
-
-This project was made as a part of the CPS843 course at TMU, Toronto, Canada.
-The model aims to review x-ray images and find out if the person has TB or not. It cannot detect any other chest diseases yet.
-
-pip install -r requirements.txt
+Gaussian Blur
+Detect Edges
+Link Edges
+Get lungs area
+Take that mask, overlap it with the actual image, set everything that is not lung mask to 0.
--- segmentation.py
+++ segmentation.py
@@ -1,15 +1,40 @@
 import numpy as np
 import cv2 as cv
+import scipy.ndimage as nd
 from matplotlib import pyplot as plt
- 
+
 img = cv.imread(r'C:\Users\reetr\OneDrive\Desktop\CPS843\Medical-Image-Segmentation-and-Classification\res\example_data\img\CHNCXR_0025_0.png', cv.IMREAD_GRAYSCALE)
-edges = cv.Canny(img,0,50)
- 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
- 
+
+#blurred = cv.GaussianBlur(img, (5, 5), 0)
+edges = cv.Canny(img, 30, 50)
+plt.imshow(edges, cmap='gray')
+plt.title('Edge Image')
 plt.show()
 
- 
+fill_im = nd.binary_fill_holes(edges).astype(np.uint8)
+plt.imshow(fill_im, cmap='gray')
+plt.title('Region Filling')
+plt.show()
+
+#elevation
+elevation_map = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
+plt.imshow(elevation_map, cmap='gray')
+plt.title('Elevation Map')
+plt.show()
+
+#markers
+markers = np.zeros_like(img, dtype=np.int32)
+markers[img < 30] = 1  # Background
+markers[img > 150] = 2  # Foreground
+
+plt.imshow(markers, cmap='jet')
+plt.title('Markers')
+plt.show()
+
+#segmentation
+segmentation = cv.watershed(cv.cvtColor(img, cv.COLOR_GRAY2BGR), markers)
+
+
+plt.imshow(segmentation, cmap='gray')
+plt.title('Segmentation')
+plt.show()
